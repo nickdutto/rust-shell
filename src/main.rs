76d17b28b9d83252e9097b0157eb6ba49508e397
nickdutto <5254128+nickdutto@users.noreply.github.com>
2026-05-27 12:38:32 +1000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
+    let builtin_commands = vec!["echo", "exit", "type"];
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -14,8 +16,16 @@ fn main() {
         if command == "exit" {
             break;
         } else if command.starts_with("echo") {
-            command = command.replace("echo", "").trim().to_string();
+            command = command[5..].trim().to_string();
             println!("{}", command);
+        } else if command.starts_with("type") {
+            command = command[5..].trim().to_string();
+            for builtin_command in &builtin_commands {
+                if *builtin_command == command {
+                    println!("{} is a shell builtin", builtin_command);
+                    break;
+                }
+            }
         } else {
             println!("{}: command not found", command);
         }
