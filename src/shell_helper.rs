ed6318c8b48e68341ebb9e74c7d0ae3081cfa396
nickdutto <5254128+nickdutto@users.noreply.github.com>
2026-06-04 13:37:00 +1000

    feat: ShellHelper filename tab completion

--- src/shell_helper.rs
+++ src/shell_helper.rs
@@ -6,6 +6,7 @@ use rustyline::highlight::Highlighter;
 use rustyline::hint::Hinter;
 use rustyline::validate::Validator;
 use rustyline::{Context, Helper};
+use std::env;
 
 pub struct ShellHelper<'a> {
     builtin_commands: Vec<&'a str>,
@@ -46,6 +47,8 @@ impl<'a> Completer for ShellHelper<'a> {
 
         if !partial_input.contains(' ') {
             self.complete_command(partial_input, &mut pos, &mut candidates);
+        } else {
+            self.complete_filename(line, &mut pos, &mut candidates);
         }
 
         candidates.sort_by(|a, b| a.replacement.cmp(&b.replacement));
@@ -76,5 +79,34 @@ impl<'a> ShellHelper<'a> {
         }
 
         *pos = 0;
+    }
+
+    fn complete_filename(&self, line: &str, pos: &mut usize, candidates: &mut Vec<Pair>) {
+        let last_partial_input = match line.split(' ').next_back() {
+            Some(last) => last.to_lowercase(),
+            None => return,
+        };
+
+        if let Some(last_whitespace) = line.rfind(' ').map(|idx| idx + 1) {
+            *pos = last_whitespace;
+        }
+
+        let filenames: Vec<String> = env::current_dir()
+            .into_iter()
+            .flat_map(|dir| dir.read_dir())
+            .flatten()
+            .filter_map(|entry| entry.ok())
+            .filter(|entry| entry.file_type().is_ok_and(|file_type| file_type.is_file()))
+            .filter_map(|entry| entry.file_name().into_string().ok())
+            .collect();
+
+        for filename in filenames {
+            if filename.to_lowercase().starts_with(&last_partial_input) {
+                candidates.push(Pair {
+                    display: filename.to_string(),
+                    replacement: format!("{} ", filename),
+                });
+            }
+        }
     }
 }
