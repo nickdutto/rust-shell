648d6eaf877c7daf3422132e5d08badb3d83e56d
nickdutto <5254128+nickdutto@users.noreply.github.com>
2026-06-10 15:33:13 +1000

    feat: print background jobs state in jobs builtin

--- src/command/builtin/jobs.rs
+++ src/command/builtin/jobs.rs
@@ -1,3 +1,32 @@
 use crate::io::tokenize::Tokens;
+use crate::io::writer::{OutputType, write_output};
+use crate::shell::ShellState;
 
-pub fn handle_jobs(tokens: Tokens) {}
+pub fn handle_jobs(tokens: Tokens, shell_state: &ShellState) {
+    let jobs_output: Vec<String> = shell_state
+        .background_jobs
+        .iter()
+        .enumerate()
+        .map(|(idx, job)| {
+            let marker = match shell_state.background_jobs.len() - idx {
+                1 => "+",
+                2 => "-",
+                _ => " ",
+            };
+
+            format!(
+                "[{}]{}  {:<24} {}",
+                job.id,
+                marker,
+                job.status.to_string(),
+                job.command
+            )
+        })
+        .collect();
+
+    write_output(
+        &jobs_output.join("\n").to_string(),
+        OutputType::Stdout,
+        &tokens,
+    );
+}
