fc886eff5f6ce8f1b4a98283c53351f90184e210
nickdutto <5254128+nickdutto@users.noreply.github.com>
2026-07-07 14:19:57 +1000

    feat: TokenScanner track current char index using CharIndices

--- src/parser/token_scanner.rs
+++ src/parser/token_scanner.rs
@@ -1,23 +1,38 @@
 use std::iter::Peekable;
-use std::str::Chars;
+use std::str::CharIndices;
 
 pub struct TokenScanner<'a> {
-    chars: Peekable<Chars<'a>>,
+    chars: Peekable<CharIndices<'a>>,
+    current_index: usize,
 }
 
 impl<'a> TokenScanner<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
-            chars: input.chars().peekable(),
+            chars: input.char_indices().peekable(),
+            current_index: 0,
         }
     }
 
+    pub fn current_index(&self) -> usize {
+        self.current_index
+    }
+
     pub fn peek(&mut self) -> Option<&char> {
-        self.chars.peek()
+        if let Some((_, ch)) = self.chars.peek() {
+            Some(ch)
+        } else {
+            None
+        }
     }
 
     pub fn next_char(&mut self) -> Option<char> {
-        self.chars.next()
+        if let Some((idx, ch)) = self.chars.next() {
+            self.current_index = idx + ch.len_utf8();
+            Some(ch)
+        } else {
+            None
+        }
     }
 
     pub fn next_if_matches(&mut self, expected: char) -> bool {
